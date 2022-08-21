50e59c0c569bd5984531d937d38eecae6d9844a6
Rafet Abdalgalil <rafetalani@gmail.com>
2022-08-20 19:06:17 -0700

    added many println statements

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        // prinln will print each argument on a separate line
         System.out.println("Hello, World!");
-
+        // \n will print each argument on a separate line
+        System.out.print("Hello, World!\n");
+        System.out.print("Hello, World!\n");
+        // \t will print each argument with a tab
+        System.out.print("\tHello, World!");
         MainFrame myFrame = new MainFrame();
         myFrame.init();
     }
